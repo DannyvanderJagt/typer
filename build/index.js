519a373ce7fd154ba96eea17face98a0d36ec82c
Danny van der Jagt <dannyvanderjagt@gmail.com>
2015-09-07 13:14:12 +0200

    [Cleanup] Removed isString, isObject and isArray, Added stream setting.

--- build/index.js
+++ build/index.js
@@ -13,29 +13,27 @@ var _util2 = _interopRequireDefault(_util);
 var _typeCheck = require('type-check');
 
 var _settings = {
-    'throw': false
+    'throw': false,
+    stream: null
 };
 
 var Typer = {
     Type: function Type(schema, obj) {
         var result = (0, _typeCheck.typeCheck)(schema, obj);
+
         if (_settings['throw'] && !result) {
             throw new Error('The type test did not pass! Schema:' + schema);
         }
+
+        if (_settings.stream) {
+            _settings.stream.emit('typeError', schema);
+        }
+
         return result;
     },
     set: function set(settings) {
         this.Type('object', settings);
         _settings = Object.assign(_settings, settings);
-    },
-    isString: function isString(obj) {
-        return _util2['default'].isString(obj);
-    },
-    isObject: function isObject(obj) {
-        return _util2['default'].isObject(obj);
-    },
-    isArray: function isArray(obj) {
-        return _util2['default'].isArray(obj);
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -2,29 +2,27 @@ import Util from 'util';
 import {typeCheck as TypeCheck} from 'type-check';
 
 let _settings = {
-    throw: false
+    throw: false,
+    stream: null
 };
 
 var Typer = {
     Type(schema, obj){
         let result = TypeCheck(schema, obj);
+        
         if(_settings.throw && !result){
             throw new Error('The type test did not pass! Schema:' + schema);
         }
+        
+        if(_settings.stream){
+            _settings.stream.emit('typeError', schema);
+        }
+        
         return result;
     },
     set(settings){
         this.Type('object', settings);
         _settings = Object.assign(_settings, settings);
-    },
-    isString(obj){
-        return Util.isString(obj);
-    },
-    isObject(obj){
-        return Util.isObject(obj);
-    },
-    isArray(obj){
-        return Util.isArray(obj);
     }
 };
 
